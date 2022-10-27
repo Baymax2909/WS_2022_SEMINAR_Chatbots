a12aa7974873e0a5fed1509ec6ea34f4b4346edf
Jonas Hillen <jonas.hillen@uni-bielefeld.de>
2022-10-27 17:37:06 +0200

    making the airbnb_bot smart together in the lecture

--- airbnb_bot/airbnb_bot.py
+++ airbnb_bot/airbnb_bot.py
@@ -1,13 +1,15 @@
 import re
 import sqlite3 as sql
+import time
 from os.path import isfile
 
+# Wir haben in der Sitzung beide gemeinsam an einem Rechner an dem Projekt gearbeitet
 
 location_regex = [
     # first entry in every tuple is a regex for matching user inputs
     # second entry in every tuple is a possible value
     #   for the key neighbourhood_group in our sql file
-    (r'(charlottenburg)|(wilmersdorf)','Charlottenburg-Wilm.'),
+    (r'(charlottenburg)|(wilmersdorf)', 'Charlottenburg-Wilm.'),
     (r'(friedrichshain)|(kreuzberg)', 'Friedrichshain-Kreuzberg'),
     (r'lichtenberg', 'Lichtenberg'),
     (r'(marzahn)|(Hellersdorf)', 'Marzahn - Hellersdorf'),
@@ -46,7 +48,6 @@ def query_sql(key, value, columns, sql_file):
     conn = sql.connect(sql_file)
     c = conn.cursor()
 
-
     # prepare query string
     query_template = 'SELECT {columns} FROM listings WHERE {key} = "{value}"'
     columns_string = ', '.join(columns)  # e.g. [location, price] -> 'location, price'
@@ -62,6 +63,35 @@ def query_sql(key, value, columns, sql_file):
     conn.close()
 
     return results
+
+# Leon Holuch & Jonas Hillen - begin #
+def ask_for_neighbourhoods():
+    # print available neighbourhoods
+    neighbourhoods = [
+        'Charlottenburg-Wilm.', 'Friedrichshain-Kreuzberg',
+        'Lichtenberg', 'Marzahn - Hellersdorf', 'Mitte', 'Neukölln', 'Pankow',
+        'Reinickendorf', 'Spandau', 'Steglitz - Zehlendorf',
+        'Tempelhof - Schöneberg', 'Treptow - Köpenick']
+    print('Wir haben Appartements in folgenden Stadtteilen:')
+    print(', '.join(neighbourhoods))
+
+    # get query from user
+    sentence = input('\nWo möchtest du denn übernachten?\n')
+    # normalize to lowercase
+    return sentence.lower()
+
+
+def ask_for_result_number():
+    u_input = input('\nWie viele Ergebnisse möchtest du sehen?\n')
+
+    try:
+        u_int = int(u_input)
+    except ValueError:
+        print('Leider ist die Eingabe von "' + u_input + '" keine Zahl!\n')
+        u_int = 10
+
+    return u_int
+# Leon Holuch & Jonas Hillen - end #
 
 
 def airbnb_bot(sql_file, top_n):
@@ -80,44 +110,27 @@ def airbnb_bot(sql_file, top_n):
         # raise an error if the file is not found
         raise FileNotFoundError(
             'Die Datei {} konnte nicht gefunden werden!'.format(sql_file)
-            )
+        )
 
-    #########################################
-    # STEP 1: say hi and ask for user input #
-    #########################################
+# Leon Holuch & Jonas Hillen - begin #
+    # ask for neighbourhood till we get a location
+    while True:
+        # ask for neighbourhoods
+        sentence = ask_for_neighbourhoods()
 
-    print('Hallöchen!\n')
+        # extract location from user input
+        location = get_location_from_input(sentence)
 
-    # print available neighbourhoods
-    neighbourhoods = [
-        'Charlottenburg-Wilm.', 'Friedrichshain-Kreuzberg',
-        'Lichtenberg', 'Marzahn - Hellersdorf', 'Mitte', 'Neukölln', 'Pankow',
-        'Reinickendorf', 'Spandau', 'Steglitz - Zehlendorf',
-        'Tempelhof - Schöneberg', 'Treptow - Köpenick']
-    print('Wir haben Appartements in folgenden Stadtteilen:')
-    print(', '.join(neighbourhoods))
+        if location is None:
+            # if the user input doesn't contain valid location information:
+            # apologize & quit
+            print('\nEntschuldigung, das habe ich leider nicht verstanden...\nVersuchen wir es nochmal!\n')
+        else:
+            break
 
-    # get query from user
-    sentence = input('\nWo möchtest du denn übernachten?\n')
-    # normalize to lowercase
-    sentence = sentence.lower()
-
-    #####################################################################
-    # STEP 2: extract location information and check whether it's valid #
-    #####################################################################
-
-
-
-    # NLU -SPRACHVERSTEHEN
-
-    # extract location from user input
-    location = get_location_from_input(sentence)
-
-    if location is None:
-        # if the user input doesn't contain valid location information:
-        # apologize & quit
-        print('\nEntschuldigung, das habe ich leider nicht verstanden...')
-        return
+    # ask how many results should be displayed
+    top_n = ask_for_result_number()
+# Leon Holuch & Jonas Hillen - end #
 
     #####################################################################
     # STEP 3: query sqlite file for flats in the area given by the user #
@@ -126,15 +139,14 @@ def airbnb_bot(sql_file, top_n):
     # get matches from csv file
     columns = ['name', 'neighbourhood', 'price']
     results = query_sql(
-            key='neighbourhood_group', value=location,
-            columns=columns, sql_file=sql_file
-        )
+        key='neighbourhood_group', value=location,
+        columns=columns, sql_file=sql_file
+    )
 
     # if there are no results: apologize & quit
     if len(results) == 0:
         print('Tut mir Leid, ich konnte leider nichts finden!')
         return
-
 
     #############################################################################
     # STEP 4: print information about the first top_n flats in the results list #
@@ -145,7 +157,8 @@ def airbnb_bot(sql_file, top_n):
     # return results
     print('Ich habe {} passende Wohnungen in {} gefunden.\n'.format(
         len(results), location))
-    print('Hier sind die {} besten Ergebnisse:\n'.format(top_n))
+    # check input ≤ num of results
+    print('Hier sind die {} besten Ergebnisse:\n'.format(min(top_n, len(results))))
 
     # print the first top_n entries from the results list
     for r in results[:top_n]:
@@ -158,4 +171,23 @@ def airbnb_bot(sql_file, top_n):
 
 if __name__ == '__main__':
     #  the airbnb_bot() function is called if the script is executed!
-    airbnb_bot(sql_file='listings.db', top_n=10)
+# Leon Holuch & Jonas Hillen - begin #
+    # greet user
+    print('Hallöchen!\n')
+    while True:
+        airbnb_bot(sql_file='listings.db', top_n=10)
+
+        u_end = input('\nMöchtest du nochmal suchen? [Ja/Nein]\n')
+
+        if u_end.lower() == 'ja':
+            continue
+        if u_end.lower() == 'nein':
+            break
+        print('\n"' + u_end + '" war keine Option... Selbstzerstörung in...')
+        print('\n...3...')
+        time.sleep(1)
+        print('...2...')
+        time.sleep(1)
+        print('...1...')
+        break
+# Leon Holuch & Jonas Hillen - end #
